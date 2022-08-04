c33f795657190ebc13e5dabced966b192def416f
AinieFatima <110605568+AinieFatima@users.noreply.github.com>
2022-08-04 18:56:00 +0500

    Add files via upload

---
+++ calculator.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+main()
+{
+	
+float a,b;
+char operation ;
+
+cout<<" Enter first number : ";
+cin>>a;
+
+cout<<" Enter Operation (+,-,*,/) : ";
+cin>>operation;
+
+cout <<" Enter second Number : ";
+cin>>b;
+
+switch(operation)
+{
+	case '+':
+		cout<<" Result = "<<a+b;
+		break;
+	case '-':
+	    cout<<" Result = "<<a-b;
+	    break;
+    case '*':
+    	cout<<" Result = "<<a*b;
+    	break;
+    case '/':
+    	cout<<" Result = "<<a/b;
+    	break;
+    default:
+    	cout<<" Invalid Operation "<<endl;
+    	
+}
+
+}
